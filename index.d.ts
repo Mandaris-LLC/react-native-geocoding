5661c81565749bd3199ff4b824f214dd98042920
ae94 <ae94@icloud.com>
2019-09-17 09:47:48 -0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,25 @@
 declare module 'react-native-geocoding' {
+    export interface Position {
+        lat: number;
+        lng: number;
+    }
+    export interface GeocodingObject {
+        formatted_address?: string;
+        geometry?: {
+            location: Position;
+        };
+        address_components: any[];
+    }
+
+    export interface GeocodingResult {
+        results?: GeocodingObject[]
+    }
+    
     export default class Geocoder {
-        init(apiKey: string, options: Object): void;
-        isInit(): boolean;
-        setApiKey(API_KEY: string): void;
-        from(...params: any[]): Promise<void>;
-        getFromLocation(address: string): Promise<any>;
-        getFromLatLng(lat: number, lng: number): Promise<any>;
+        static init(apiKey: string, options?: Object): void;
+        static isInit(): boolean;
+        static from(...params: any[]): Promise<void>;
+        static getFromLocation(address: string): Promise<GeocodingResult>;
+        static getFromLatLng(lat: number, lng: number): Promise<GeocodingResult>;
     }
 }
